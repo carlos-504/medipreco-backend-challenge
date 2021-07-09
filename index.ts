795a1f4ea0dc9323a778bea081fd35b31be94770
carlos-504 <carllosshennriique@gmail.com>
2021-07-09 14:05:59 -0300

    feat: connect api with database

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import db from './src/models';
 
 const app = express();
 const port = 3003;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
+  })
+  .catch((err: Error) => console.log(err));
